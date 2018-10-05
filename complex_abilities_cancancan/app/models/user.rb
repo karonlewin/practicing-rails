9caf58f2c1634328a14a536d6957e54c6267fb17
Karon Lewin <karonlewin@gmail.com>
2018-10-05 14:18:12 -0300

    Removing unused code

--- complex_abilities_cancancan/app/models/user.rb
+++ complex_abilities_cancancan/app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :user_permissions
-  has_many :sectors, through: :user_permissions
 end
